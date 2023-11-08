c8c380fb40ecb51c7ff15730af34d6f70bde0b5d
Artem Slubskyi <slubsku2.0@gmail.com>
2023-11-08 15:58:14 +0100

    Added cors

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose")
 const express = require("express")
+const cors = require('cors')
 const todoRouter = require("./routers/todoRouter.js");
 const authRouter = require("./routers/authRouter.js")
 const app = express()
 const PORT = process.env.PORT || 5555
 const DB_URL = "mongodb+srv://artemslubskiy:1234@mega-todo-claster.vuvqb22.mongodb.net/?retryWrites=true"
 
-
+app.use(cors())
 app.use(express.json())
 
 app.use("/api" , todoRouter)
